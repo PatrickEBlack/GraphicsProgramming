ec0d53fa8e6362d6505fe50345d9cc868f5be21a
PatrickEBlack <g00410388@atu.ie>
2023-12-04 10:20:35 +0000

    Lab8_Canny

--- Lab8/EdgeDetection.py
+++ Lab8/EdgeDetection.py
@@ -5,7 +5,7 @@ from matplotlib import pyplot as plt
 
 # Number of Rows and Columns
 nrows = 2
-ncols = 2
+ncols = 3
 
 # GrayScale
 imgOrig = cv2.imread('ATU.jpg')
@@ -16,11 +16,12 @@ img13by13 = cv2.GaussianBlur(imgGray,(13, 13), 0)
 # Edge Detection
 sobelHorizontal = cv2.Sobel(imgGray, cv2.CV_64F, 1, 0, ksize = 5) # x direction
 sobelVertical = cv2.Sobel(imgGray, cv2.CV_64F, 0, 1, ksize = 5) # y direction
+canny = cv2.Canny(imgGray, 100, 300)
 
 # Display Window - Matplotlib as Plt
-plt.subplot(nrows, ncols, 1), plt.imshow(cv2.cvtColor(imgOrig,cv2.COLOR_BGR2RGB), cmap = 'gray')
+plt.subplot(nrows, ncols, 1), plt.imshow(cv2.cvtColor(imgOrig,cv2.COLOR_BGR2RGB), cmap = 'gray') # Original
 plt.title('Original'), plt.xticks([]), plt.yticks([])
-plt.subplot(nrows, ncols, 2), plt.imshow(imgGray, cmap = 'gray')
+plt.subplot(nrows, ncols, 2), plt.imshow(imgGray, cmap = 'gray') # GrayScale
 plt.title('GrayScale'), plt.xticks([]), plt.yticks([])
 
                 # BLURRED IMAGES #
@@ -29,10 +30,14 @@ plt.title('GrayScale'), plt.xticks([]), plt.yticks([])
 # plt.subplot(nrows, ncols, 4), plt.imshow(img13by13, cmap = 'gray')
 # plt.title('13 x 13'), plt.xticks([]), plt.yticks([])
 
-plt.subplot(nrows, ncols, 3), plt.imshow(sobelHorizontal, cmap = 'gray')
+plt.subplot(nrows, ncols, 3), plt.imshow(sobelHorizontal, cmap = 'gray') # Sobel X
 plt.title('Sobel X'), plt.xticks([]), plt.yticks([])
-plt.subplot(nrows, ncols, 4), plt.imshow(sobelVertical, cmap = 'gray')
+plt.subplot(nrows, ncols, 4), plt.imshow(sobelVertical, cmap = 'gray') # Sobel Y
 plt.title('Sobel Y'), plt.xticks([]), plt.yticks([])
+plt.subplot(nrows, ncols, 5), plt.imshow(sobelHorizontal + sobelVertical, cmap = 'gray') # Sobel Sum
+plt.title('Sobel Sum'), plt.xticks([]), plt.yticks([])
+plt.subplot(nrows, ncols, 6), plt.imshow(canny, cmap = 'gray') # Canny Edge Detection
+plt.title('Canny Edge Image'), plt.xticks([]), plt.yticks([])
 
 plt.show()
 
